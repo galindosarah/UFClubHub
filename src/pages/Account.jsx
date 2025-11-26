168fff937b111d41ed02e7f9ffbad7c99be261d3
celine-hui <ccelinehui@gmail.com>
2025-11-26 13:03:24 -0500

    Added buttons to Account and individual Club pages to demo functionality

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import "./Account.css"
 
 export default function Account() {
+  const navigate = useNavigate();
+
+  const goToStart = () => {
+    navigate("/login");
+  };
+
   return ( 
   <div> 
     <div className="header">
       <h1>My Account</h1>
-      <button type="submit" className="logout-button">Logout</button>
+      <button onClick={goToStart} className="logout-button">Logout</button>
     </div>
     
     <div className="profile-wrap">
--- src/pages/ClubPage.jsx
+++ src/pages/ClubPage.jsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./ClubPage.css"
 
 export default function ClubPage() {
-  const { clubId } = useParams();
-  const [club, setClub] = useState(null);
+    const { clubId } = useParams();
+    const [club, setClub] = useState(null);
 
 //   useEffect(() => {
 //     fetch(`http://localhost:8000/api/clubs/${clubId}/`)
@@ -11,19 +12,54 @@ export default function ClubPage() {
 //       .then(data => setClub(data));
 //   }, [clubId]);
 
+    // const handleJoinLeave = () => {
+    //     setClub(prev => ({
+    //     ...prev,
+    //     is_member: !prev.is_member  
+    //     }));
+    // };
+
+    //Demo joining/leaving club with no backend yet
+    const [isMember, setIsMember] = useState(false);
+    const demoJoinLeave = () => {
+        setIsMember((prev) => !prev);
+    }
+
+    if (!club) return (
+        <div>
+            <div className="header">
+                <h1>Club Name</h1>
+                <button 
+                    className={isMember ? "leave-button" : "join-button"}
+                    onClick={demoJoinLeave}
+                >
+                    {isMember ? "Leave Club" : "Join Club"}
+                </button>
+            </div>
+
+            <div className="info-wrap">
+                <h2>About</h2>
+                <p> To educate all UF students on the animation industry and animation as an art form, to provide networking, 
+                    job, learning, and other opportunities to all students, and bridge the gap between online and in-person 
+                    students interested in animation.</p>
+        
+
+            </div>
+        </div>
+    );;
 
 
-  if (!club) return <p>Loading...</p>;
+    return (
+        <div>
+            <h1>{club.name}</h1>
+            <p>{club.bio}</p>
 
-
-  return (
-    <div>
-      <h1>{club.name}</h1>
-      <p>{club.bio}</p>
-
-      <button onClick={handleJoinLeave}>
-        {club.is_member ? "Leave Club" : "Join Club"}
-      </button>
-    </div>
-  );
+            <button 
+                className={club.is_member ? "leave-button" : "join-button"}
+                onClick={handleJoinLeave}
+            >
+                {club.is_member ? "Leave Club" : "Join Club"}
+            </button>
+        </div>
+    );
 }
